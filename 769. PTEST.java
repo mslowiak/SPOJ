572e8cc523db0e4e890f344bb7ed414429d9f2b0
mq007 <marcin.slowiak.007@gmail.com>
2017-03-01 12:52:18 +0100

    Rozwiązanie zadania nr. 769

---
+++ 769. PTEST.java
@@ -0,0 +1,16 @@
+/*
+    Marcin Słowiak
+    rozwiązanie zadania nr. 769 - http://pl.spoj.com/problems/PTEST/
+*/
+
+import java.util.*;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
