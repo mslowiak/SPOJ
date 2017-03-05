b63eeb3641f5e3337c860af040fe6b10a60ea04f
mq007 <marcin.slowiak.007@gmail.com>
2017-03-05 20:37:39 +0100

    Rozwiązanie zadania nr. 1828

---
+++ 1828. KC001.java
@@ -0,0 +1,18 @@
+﻿/*
+    Marcin Słowiak
+    rozwiązanie zadania nr. 1828 - http://pl.spoj.com/problems/KC001/
+*/
+
+import java.util.*;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i=0; i<3; ++i){
+            sum = sum + in.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
