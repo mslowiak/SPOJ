4f1cead497a080b9d9327f5c1ca6134c9ec80bdf
mq007 <marcin.slowiak.007@gmail.com>
2017-03-05 20:37:48 +0100

    Rozwiązanie zadania nr. 1830

---
+++ 1830. KC003.java
@@ -0,0 +1,24 @@
+﻿/*
+    Marcin Słowiak
+    rozwiązanie zadania nr. 1830 - http://pl.spoj.com/problems/KC003/
+*/
+
+import java.util.*;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        double a, b, c, cMax;
+        while(in.hasNextDouble()){
+            a = in.nextDouble();
+            b = in.nextDouble();
+            c = in.nextDouble();
+            if((a+b>c) && (c+b>a) && (a+c>b)){
+                System.out.println("1");
+            }else{
+                System.out.println("0");
+            }
+        }
+    }
+}
