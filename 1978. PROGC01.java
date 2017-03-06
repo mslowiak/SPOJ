6698511fcb1418b476a669b0d62241b36069c6e6
mq007 <marcin.slowiak.007@gmail.com>
2017-03-07 00:43:26 +0100

    Rozwiązanie zadania nr. 1978

---
+++ 1978. PROGC01.java
@@ -0,0 +1,19 @@
+﻿/*
+    Marcin Słowiak
+    rozwiązanie zadania nr. 1978 - http://pl.spoj.com/problems/PROGC01/
+*/
+
+import java.util.*;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args){
+        int amountOfLines = 0;
+        while(in.hasNextLine()){
+            in.nextLine();
+            amountOfLines++;
+        }
+        System.out.println(amountOfLines);
+    }
+}
