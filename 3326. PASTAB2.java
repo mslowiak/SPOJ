522fdfbaa5d669bcfbbce6f9ef729fb35b390307
mq007 <marcin.slowiak.007@gmail.com>
2017-03-07 17:52:05 +0100

    Rozwiązanie zadania nr. 3326

---
+++ 3326. PASTAB2.java
@@ -0,0 +1,33 @@
+﻿/*
+    Marcin Słowiak
+    rozwiązanie zadania nr. 3326 - http://pl.spoj.com/problems/PASTAB2/
+*/
+
+import java.util.*;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int amountOfData = in.nextInt();
+        int [] array = new int[amountOfData];
+        for(int i=0; i<amountOfData; ++i){
+            array[i] = in.nextInt();
+        }
+        char ch = in.next().charAt(0);
+        int value = in.nextInt();
+        if(ch == '>'){
+            for(int i=0; i<amountOfData; ++i){
+                if(array[i] > value){
+                    System.out.println(array[i]);
+                }
+            }
+        }else if(ch == '<'){
+            for(int i=0; i<amountOfData; ++i){
+                if(array[i] < value){
+                    System.out.println(array[i]);
+                }
+            }
+        }
+    }
+}
