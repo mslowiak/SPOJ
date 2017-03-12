f0e462b8a1211eda0c73cccf29948d934a9fa3a4
mq007 <marcin.slowiak.007@gmail.com>
2017-03-13 00:00:32 +0100

    Rozwiązanie zadania nr. 21021

--- 21021. FR_02_06.java
+++ 21021. FR_02_06.java
@@ -9,24 +9,40 @@ public class Main {
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String x1 = in.nextLine();
-        String x2 = in.nextLine();
-        String x3 = in.nextLine();
-        int result = 0;
-        for(int i=0; i<x1.length(); ++i){
-            char ch1 = x1.charAt(i);
-            char ch2 = x2.charAt(i);
-            char ch3 = x3.charAt(i);
-            if(ch1 != ch2 && ch2 == ch3){
-                result = result + 2;
-            }else if(ch1 != ch2 && ch1 == ch3){
-                result++;
-            }else if(ch1 != ch3 && ch1 == ch2){
-                result++;
-            }else if(ch1 != ch2 && ch1 != ch3){
-                result++;
+        int amountOfData = in.nextInt();
+        List<String> okey = new ArrayList<>();
+        List<String> niedowaga = new ArrayList<>();
+        List<String> nadwaga = new ArrayList<>();
+        for(int i=0; i<amountOfData; ++i){
+            String name = in.next();
+            double weight = in.nextDouble();
+            double height = in.nextDouble();
+            height = height/100;
+            double bmi = weight/(height*height);
+            if(bmi >= 25){
+                nadwaga.add(name);
+            }else if(bmi > 18.5){
+                okey.add(name);
+            }else{
+                niedowaga.add(name);
             }
         }
-        System.out.println(result);
+
+        System.out.println("niedowaga");
+        for(String x : niedowaga){
+            System.out.println(x);
+        }
+        System.out.println("");
+
+        System.out.println("wartosc prawidlowa");
+        for(String x : okey){
+            System.out.println(x);
+        }
+        System.out.println("");
+
+        System.out.println("nadwaga");
+        for(String x : nadwaga){
+            System.out.println(x);
+        }
     }
 }
